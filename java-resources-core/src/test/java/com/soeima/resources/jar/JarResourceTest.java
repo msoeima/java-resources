38a77d06cd9627a2c9ab08b115fc990205769b27
marco.soeima <marco.soeima@gmail.com>
2012-10-05 22:25:18 +0100

    Fix the unit test so that it properly works on Unix systems as well as on Windows.

--- java-resources-core/src/test/java/com/soeima/resources/jar/JarResourceTest.java
+++ java-resources-core/src/test/java/com/soeima/resources/jar/JarResourceTest.java
@@ -54,7 +54,7 @@ public class JarResourceTest extends AbstractResourceTest {
      * @see  AbstractResourceTest#toURL(String)
      */
     @Override protected String toURL(String path) {
-        return "jar:file:/" + Paths.normalize(path, '/') + "!/";
+        return Paths.normalize(Paths.join("jar:file:" + path), '/') + "!/";
     }
 
     /**
